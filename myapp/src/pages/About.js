81ef649395db58e1028c8b3dde3e14e514e26951
Atandrila Anuva <atandrilaanuva@gmail.com>
2023-04-23 22:35:10 -0400

    Week 3 deliverables - Basic Components + Relationship Diagram

---
+++ myapp/src/pages/About.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function About() {
+  return (
+    <>
+    <h1>
+        Info about me.
+    </h1>
+    </>
+  );
+}
---
+++ myapp/src/pages/RepoList.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function RepoList() {
+  return (
+    <>
+    <h1>
+        Repo list will go here.
+    </h1>
+    </>
+  );
+}
